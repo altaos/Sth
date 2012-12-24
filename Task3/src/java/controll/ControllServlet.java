ccc997602dd6d1bce9130ac11b20773a9a1f5edf
altaos <alishka.man@gmail.com>
2012-12-24 17:46:34 +0400

    bad try

--- Task3/src/java/controll/ControllServlet.java
+++ Task3/src/java/controll/ControllServlet.java
@@ -4,7 +4,18 @@
  */
 package controll;
 
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
+import com.j256.ormlite.jdbc.JdbcConnectionSource;
+import com.j256.ormlite.jdbc.JdbcPooledConnectionSource;
+import com.j256.ormlite.stmt.QueryBuilder;
+import com.j256.ormlite.stmt.Where;
+import database.Player;
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -34,35 +45,64 @@ public class ControllServlet extends HttpServlet
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException
     {
-        String userPath = request.getServletPath();
-        String url = null;
-        
-        if (userPath.equals("/autoriation"))
-        {
-            //TODO: работа с бд
-        } 
-        else if(userPath.equals("/registration"))
-        {
-            url = "/WEB-INF/view" + userPath + ".jsp";
-        }
-        else if(userPath.equals("/cancel"))
-        {
-            url = "index.jsp";
-        }
-        else
-        {
-            url = "/WEB-INF/view" + userPath + ".jsp";
-        }
-        
-        //String url = "/WEB-INF/view" + userPath + ".jsp";
         
         try
         {
-            request.getRequestDispatcher(url).forward(request, response);
-        } catch (Exception ex)
+            //try
+            //{
+            String userPath = request.getServletPath();
+            String url = null;
+            JdbcConnectionSource connectionSource;
+            Dao<Player,Integer> playerDao;
+            
+            connectionSource = new JdbcPooledConnectionSource("jdbc:mysql://localhost:3306/checkers", "root", "");
+            playerDao = DaoManager.createDao(connectionSource, Player.class);
+            
+            if (userPath.equals("/autorization"))
+            {
+                //QueryBuilder<Player, Integer> queryBuilder = playerDao.queryBuilder();
+                List<Player> player = playerDao.queryBuilder().where()
+                        .eq(Player.NAME_FIELD_NAME, request.getAttribute("Login"))
+                        .and()
+                        .eq(Player.PASSWORD_FIELD_NAME, request.getAttribute("Password")).query();
+                
+                if(player.size() == 1)
+                {
+                    userPath = "/registration";
+                    url = "/WEB-INF/view" + userPath + ".jsp";
+                }
+                //TODO: работа с бд
+            } 
+            else if(userPath.equals("/registration"))
+            {
+                url = "/WEB-INF/view" + userPath + ".jsp";
+            }
+            else if(userPath.equals("/cancel"))
+            {
+                url = "index.jsp";
+            }
+            else
+            {
+                url = "/WEB-INF/view" + userPath + ".jsp";
+            }
+            
+            //String url = "/WEB-INF/view" + userPath + ".jsp";
+            
+            try
+            {
+                request.getRequestDispatcher(url).forward(request, response);
+            } catch (Exception ex)
+            {
+                ex.printStackTrace();
+            }
+            //}
+            //catch()
+        } catch (SQLException ex)
         {
-            ex.printStackTrace();
+            Logger.getLogger(ControllServlet.class.getName()).log(Level.SEVERE, null, ex);
         }
+        //}
+        //catch()
     }
 
     @Override
--- Task3/src/java/database/DB.java
+++ Task3/src/java/database/DB.java
@@ -9,18 +9,28 @@ import com.j256.ormlite.jdbc.JdbcPooledConnectionSource;
 import java.sql.SQLException;
 
 /**
- *
+ * тестовая версия, доделать сессии, убрать новые подключения
  * @author Alina
  */
 public class DB
 {
-    private static final String databaseUrl = "jdbc:mysql://vragov.com:3306/checkers";
+    private static final String databaseUrl = "jdbc:mysql://localhost:3306/checkers";
+    private JdbcConnectionSource connectionSource;
+    
+    //переделать!!! 
+    public DB() { }
+    
+    public JdbcConnectionSource getConnectionSource()
+    {
+        return connectionSource;
+    }
     
     public JdbcConnectionSource CreateConnectionSource() throws SQLException
     {
         try
         {
-            JdbcConnectionSource connectionSource = new JdbcPooledConnectionSource(databaseUrl);
+            connectionSource = new JdbcPooledConnectionSource(databaseUrl, "root", "");
+            //Dao<Player,Integer> playerDao = DaoManager.createDao(connectionSource, Player.class);
             return connectionSource;
         }
         catch(SQLException exception)

c311dabdd2b670a762b94f26785c10152da9e18e
altaos <alishka.man@gmail.com>
2012-12-28 01:54:56 +0400

    Изменения

--- Task3/src/java/controll/ControllServlet.java
+++ Task3/src/java/controll/ControllServlet.java
@@ -12,10 +12,12 @@ import com.j256.ormlite.stmt.PreparedQuery;
 import com.j256.ormlite.stmt.QueryBuilder;
 import com.j256.ormlite.stmt.SelectArg;
 import com.j256.ormlite.stmt.Where;
+import database.Country;
 import database.Player;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.ServletException;
@@ -32,7 +34,10 @@ import javax.servlet.http.HttpServletResponse;
         loadOnStartup = 1,
         urlPatterns ={"/autorization", 
                       "/registration",
-                      "/cancel"
+                      "/cancel",
+                      "/sendReg",
+                      "/saveChanges",
+                      "/cancelChanges"
                                      })
 public class ControllServlet extends HttpServlet
 {
@@ -50,15 +55,17 @@ public class ControllServlet extends HttpServlet
         
         try
         {
-            //try
-            //{
             String userPath = request.getServletPath();
             String url = null;
             JdbcConnectionSource connectionSource;
             Dao<Player,Integer> playerDao;
+            Dao<Country,Integer> countryDao;
+            request.setCharacterEncoding("utf-8");
+            response.setCharacterEncoding("utf-8");
             
-            connectionSource = new JdbcConnectionSource("jdbc:mysql://localhost:3306/checkers", "root", "");
+            connectionSource = new JdbcConnectionSource("jdbc:mysql://localhost:3306/checkers?useUnicode=true&characterEncoding=utf8", "root", "");
             playerDao = DaoManager.createDao(connectionSource, Player.class);
+            countryDao = DaoManager.createDao(connectionSource, Country.class);
             
             if (userPath.equals("/autorization"))
             {
@@ -69,7 +76,8 @@ public class ControllServlet extends HttpServlet
                 PreparedQuery<Player> preparedQuery = queryBuilder.prepare();
                 selectArg.setValue(request.getParameterValues("login")[0]);
                 List<Player> list = playerDao.query(preparedQuery);*/
-                
+                String name = request.getParameterValues("login")[0];
+                String pass = request.getParameterValues("pass")[0];
                 List<Player> player = playerDao.queryBuilder().where()
                         .eq(Player.NAME_FIELD_NAME, request.getParameterValues("login")[0])
                         .and()
@@ -77,7 +85,9 @@ public class ControllServlet extends HttpServlet
                 
                 if(player.size() == 1)
                 {
-                    userPath = "/registration";
+                    request.getSession().setAttribute("login", request.getParameterValues("login")[0]);
+                    request.getSession().setAttribute("user_id", player.get(0).getId());
+                    userPath = "/changeInfo";
                     url = "/WEB-INF/view" + userPath + ".jsp";
                 }
                 else
@@ -94,29 +104,65 @@ public class ControllServlet extends HttpServlet
             {
                 url = "index.jsp";
             }
+            else if(userPath.equals("/sendReg"))
+            {
+                List<Player> player = playerDao.queryBuilder().where()
+                        .eq(Player.NAME_FIELD_NAME, request.getParameterValues("Login")[0])
+                        .and()
+                        .eq(Player.PASSWORD_FIELD_NAME, request.getParameterValues("Password")[0]).query();
+                
+                int c_id = countryDao.queryBuilder().where().eq(Country.COUNTRY_NAME, request.getParameterValues("Country")[0]).query().get(0).getId();
+                
+                if(player.size() == 0)
+                {
+                    playerDao.create(new Player(request.getParameterValues("Login")[0], request.getParameterValues("Password")[0], request.getParameterValues("Name")[0], request.getParameterValues("Email")[0], c_id));
+                }
+                else
+                {
+                    request.setAttribute("er", "A user with this login is already registered");
+                    url = "/registration";                    
+                }
+            }
+            else if(userPath.equals("/sendReg"))
+            {
+                Player pl = playerDao.queryForId((Integer)request.getSession().getAttribute("user_id"));
+                pl.setActual_name(request.getParameterValues("Name")[0]);
+                pl.setEmail(request.getParameterValues("Email")[0]);
+                pl.setId_country(countryDao.queryBuilder().where().eq(Country.COUNTRY_NAME, request.getParameterValues("Country")[0]).query().get(0).getId());
+                if(pl.getPassword().equals(request.getParameterValues("Password_old")[0]))
+                {
+                    pl.setPassword(request.getParameterValues("Password_new")[0]);
+                }
+                else
+                {
+                    request.setAttribute("wrong_pass", "Old password is wrong");
+                    url = "/WEB-INF/view/changeInfo.jsp";
+                }
+                playerDao.update(pl);
+            }
+            else if(userPath.equals("/cancelChanges"))
+            {
+                //ссылка на страницу, с которой вызывали изменение профиля
+                url = "/country";
+            }
             else
             {
                 url = "/WEB-INF/view" + userPath + ".jsp";
             }
             
-            //String url = "/WEB-INF/view" + userPath + ".jsp";
-            
             try
             {
-                //request.setAttribute("par", "Hahaha");
                 request.getRequestDispatcher(url).forward(request, response);
             } catch (Exception ex)
             {
                 ex.printStackTrace();
             }
-            //}
-            //catch()
+            
         } catch (SQLException ex)
         {
             Logger.getLogger(ControllServlet.class.getName()).log(Level.SEVERE, null, ex);
         }
-        //}
-        //catch()
+        
     }
 
     @Override
